6f4c15e847f663c3d8d945136afe513d538548b0
Daniil Vorontsov <danv2014@gmail.com>
2021-10-19 16:22:14 +0300

    Point class with 2 points and distance function was created

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,10 +1,17 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public int x;
-  public int y;
-  public Point (int x, int y) {
-    this.x=x;
-    this.y=y;
+  public int x1;
+  public int x2;
+  public int y1;
+  public int y2;
+  public Point (int x1,int y1, int x2,int y2) {
+    this.x1=x1;
+    this.y1=y1;
+    this.x2=x2;
+    this.y2=y2;
+  }
+  public double distance () {
+    return Math.sqrt(Math.pow(x1-x2,2)+ Math.pow(y1-y2,2));
   }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgram.java
@@ -2,11 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class PointProgram {
   public static void main(String[] args) {
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(4,5);
-    System.out.println("Расстояние от точки ("+p1.x+","+p1.y+") до точки ("+p2.x+","+p2.y+")="+distance(p1,p2));
+    Point p1 = new Point(1,1,4,5);
+    System.out.println("Расстояние от точки ("+p1.x1+","+p1.y1+") до точки ("+p1.x2+","+p1.y2+")="+p1.distance());
   }
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt(Math.pow(p1.x- p2.x,2)+ Math.pow(p1.y-p2.y,2));
-  }
+
 }
