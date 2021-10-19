8bc05ad11bec2bc37ac70e13e0550e51d51d8b7f
Daniil Vorontsov <danv2014@gmail.com>
2021-10-19 15:58:09 +0300

    function was created!

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,10 +9,17 @@ public class MyFirstProgram {
     System.out.println("Площадь квадрата со стороной "+s.l+"="+s.area());
     Rectangle r = new Rectangle(4,6);
     System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = "+r.area());
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(4,5);
+    System.out.println("Расстояние от точки ("+p1.x+","+p1.y+") до точки ("+p2.x+","+p2.y+")="+Math.pow(p1.x- p2.x,2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, "+somebody+"!");
+  }
+
+  public static double distance (Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p1.x- p2.x,2)+ Math.pow(p1.y-p2.y,2));
   }
 
 
