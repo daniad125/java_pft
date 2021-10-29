21335df7e995c387c520594057f27d01d5c2120e
Daniil Vorontsov <danv2014@gmail.com>
2021-10-29 14:10:40 +0300

    Other order of IF

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -10,30 +10,28 @@ public class Equation {
         this.b=b;
         this.c=c;
         double d=b*b-4*a*c;
-
-        if (a==0) {
-            System.out.println("Вырожденное уравнение");
-            if (b==0) {
-                if (c==0) {
-                    n=-1;
-                }
-                else {
-                    n=0;
-                }
+        if (a!=0) {
+            if (d > 0) {
+                n = 2;
+            }
+            else if (d == 0) {
+                n = 1;
             }
             else {
-                n=1;
+                n = 0;
             }
         }
-        else if (d>0) {
-            n=2;
-        }
-        else if (d==0) {
-                n=1;
+        else if (b==0) {
+            if (c==0) {
+                n=-1;
             }
             else {
                 n=0;
             }
+        }
+        else {
+            n=1;
+        }
     }
     public int rootNumber() {
         return n;
