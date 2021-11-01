f6e7f79732b590ab22345800e10d52da8c0b49b2
Daniil Vorontsov <danv2014@gmail.com>
2021-11-01 13:29:13 +0300

    Prime tests were created

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class PrimeTests {
+    @Test
+    public void testPrime1() {
+        Assert.assertTrue(Primes.isPrime(7));
+    }
+    @Test
+    public void testPrime2() {
+        Assert.assertTrue(Primes.isPrime(2));
+    }
+    @Test
+    public void testPrime3() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+    @Test
+    public void testNonPrime1(){
+        Assert.assertFalse(Primes.isPrime(18));
+    }
+    @Test
+    public void testNonPrime2() {
+        Assert.assertFalse(Primes.isPrime(2965));
+    }
+    @Test
+    public void testNonPrime3() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+}
