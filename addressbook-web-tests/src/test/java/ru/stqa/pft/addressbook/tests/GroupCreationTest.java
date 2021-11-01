851e705c1b783900afab2bc9fcdb7bec94cc7e9c
Daniil Vorontsov <danv2014@gmail.com>
2021-11-01 22:49:02 +0300

    Liambda was added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -22,20 +22,7 @@ public class GroupCreationTest extends TestBase{
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(),after.size()-1);
-        int max=0;
-        for (GroupData g: after){
-            if(g.getId()>max) {
-                max=g.getId();
-            }
-        }
-        Comparator<? super GroupData> byId= new Comparator<GroupData>() {
-            @Override
-            public int compare(GroupData o1, GroupData o2) {
-                return Integer.compare(o1.getId(), o2.getId());
-            }
-        } ;
-        int max1 = after.stream().max(byId).get().getId();
-        group.setId(max1);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
