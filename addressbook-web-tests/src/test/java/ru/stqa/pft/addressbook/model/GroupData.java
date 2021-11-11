154080818d687a6e689cb4cc5276b893b3f029cd
Daniil Vorontsov <danv2014@gmail.com>
2021-11-11 21:51:40 +0300

    JSON generator and test was created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import jdk.jfr.Experimental;
 
 import java.util.Objects;
 
@@ -10,10 +12,11 @@ public class GroupData {
     @XStreamOmitField
     private int id=Integer.MAX_VALUE;
 
-
-
+    @Expose
     private String name;
+    @Expose
     private String header;
+    @Expose
     private String footer;
 
     public GroupData withId(int id) {
